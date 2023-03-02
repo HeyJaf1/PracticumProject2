e44adee7aaa7501920a8f2c5fa8de05c5f2e3528
Ildar Yafarov <heyjaf13@yandex.ru>
2023-03-02 12:46:08 +0300

    корректный номер

--- src/test/java/OrderTest.java
+++ src/test/java/OrderTest.java
@@ -42,8 +42,10 @@ public class OrderTest {
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+
 //        WebDriverManager.firefoxdriver().setup();
 //        driver = new FirefoxDriver();
+
         MainPage webScooterMainPage = new MainPage(driver);
         webScooterMainPage.openMainPage();
 
